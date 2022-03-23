7aceebe8a7d9a51b5a05505629128cfafa71b3ac
realiti-a <98154578+realiti-a@users.noreply.github.com>
2022-03-23 16:38:25 +0900

    [level 2] Title: 피보나치 수, Time: 3.36 ms, Memory: 7.61 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.cpp
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.cpp
@@ -3,15 +3,11 @@
 
 using namespace std;
 
-vector<int> fibNum;
+vector<int> fibNum = {0, 1};
 
 int solution(int n) {
-    fibNum.push_back(0);
-    fibNum.push_back(1);
-    
-    for(int i = 2; i <= n; i++){
-        fibNum.push_back((fibNum[i - 2] + fibNum[i - 1])%1234567);
+    if(n >= fibNum.size()){
+        fibNum.push_back((solution(n - 2) + solution(n - 1)) % 1234567);
     }
-    
     return fibNum[n];
 }
