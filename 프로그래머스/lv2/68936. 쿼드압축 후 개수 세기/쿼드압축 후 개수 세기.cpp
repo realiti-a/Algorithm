70136b03d722fb9e6511a999ec0f8e04c0d4d5b2
realiti-a <98154578+realiti-a@users.noreply.github.com>
2022-04-06 17:17:45 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 1.74 ms, Memory: 14.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> result;
+vector<vector<int>> map;
+
+void press(int dy, int dx, int size){
+    if(size == 1){
+        result.push_back(map[dy][dx]);
+        return;
+    }
+    
+    int flag = 0;
+    int ans = map[dy][dx];
+    for(int y = dy; y < dy + size; y++){
+        for(int x = dx; x < dx + size; x++){
+            if(map[y][x] != ans) {
+                flag = 1;
+                break;
+            }
+        }
+        if(flag) break;
+    }
+    
+    if(!flag) result.push_back(ans);
+    else {
+        press(dy, dx, size/2);
+        press(dy + size/2, dx, size/2);
+        press(dy, dx + size/2, size/2);
+        press(dy + size/2, dx + size/2, size/2);
+    }
+}
+
+
+vector<int> solution(vector<vector<int>> arr) {
+    vector<int> answer(2, 0);
+    map = arr;
+    
+    press(0, 0, arr.size());
+    
+    for(int i = 0; i < result.size(); i++){
+        if(result[i]) answer[1]++;
+        else answer[0]++;
+    }
+    return answer;
+}
