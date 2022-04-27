4184a804f94a0a0b6d149d6322d78ee242bd796b
realiti-a <98154578+realiti-a@users.noreply.github.com>
2022-04-27 20:39:44 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int answer = 0;
+    int total_weight = 0;
+    queue<int> bridge;
+    
+    for(int i = 0; i < bridge_length; i++) bridge.push(0);
+    
+    for(int i = 0; i < truck_weights.size(); i++){
+        int next = truck_weights[i];
+        
+        while(true){
+            answer++;
+            total_weight -= bridge.front();
+            bridge.pop();
+            
+            if(total_weight + next <= weight) break;
+            bridge.push(0);
+        }
+        
+        bridge.push(next);
+        total_weight += next;
+    }
+    
+    while(!bridge.empty()) {
+        answer++;
+        bridge.pop();
+    }
+    
+    return answer;
+}
