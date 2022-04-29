a0e8a8e8755644cd16ec9889a580d0e63ab380e1
realiti-a <98154578+realiti-a@users.noreply.github.com>
2022-04-29 10:14:15 +0900

    [Silver II] Title: 최소 힙, Time: 68 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+priority_queue< int, vector<int>, greater<int> > q;
+vector<int> res;
+
+int main(){
+    int N;
+    cin >> N;
+
+    int order;
+    for(int i = 0; i < N; i++){
+        cin >> order;
+
+        if(order == 0) {
+            if(q.empty()) res.push_back(0);
+            else {
+                res.push_back(q.top());
+                q.pop();
+            }   
+        }   
+        else q.push(order);
+    }   
+
+    for(auto i: res){
+        cout << i << "\n";
+    }   
+
+    return 0;
+}
