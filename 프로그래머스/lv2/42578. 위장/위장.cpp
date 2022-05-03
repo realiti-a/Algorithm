b78856e761e36a8260aa089ce9e9adc85e58e5ec
realiti-a <98154578+realiti-a@users.noreply.github.com>
2022-05-03 10:46:46 +0900

    [level 2] Title: 위장, Time: 0.02 ms, Memory: 4.04 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.cpp
+++ 프로그래머스/lv2/42578. 위장/위장.cpp
@@ -17,9 +17,10 @@ int solution(vector<vector<string>> clothes) {
     }
     
     int answer = 1;
+    //각 옷 종류마다 아예 안입기, 하나씩 꺼내 입기 의 선택지가 있어 사이즈보다 하나의 경우의 수가 더 있다.
     for(auto iter: closet){
         answer *= iter.second.size() + 1;
     }
-    
+    //대신에 모두 안 입는 경우는 제외해 줘야 한다.
     return answer - 1;
 }
