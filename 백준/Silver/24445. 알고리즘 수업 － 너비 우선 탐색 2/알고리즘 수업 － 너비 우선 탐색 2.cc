76fc579170175a824ce1c09276dc84bc396178c0
realiti-a <98154578+realiti-a@users.noreply.github.com>
2022-05-09 22:16:59 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 216 ms, Memory: 8708 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> edges;
+vector<int> visited;
+int N, M, R;
+void input() {
+	cin >> N >> M >> R;
+
+	edges = vector<vector<int>>(N + 1, vector<int>(0));
+	visited = vector<int>(N + 1, 0);
+
+	int from, to;
+	for (int i = 0; i < M; i++) {
+		cin >> from >> to;
+
+		edges[from].push_back(to);
+		edges[to].push_back(from);
+	}
+}
+
+int cnt = 0;
+void bfs() {
+	queue<int> q;
+
+	q.push(R);
+	visited[R] = ++cnt;
+
+	while (!q.empty()) {
+		int now = q.front();
+		q.pop();
+
+		for (int i = 0; i < edges[now].size(); i++) {
+			int to = edges[now][i];
+
+			if (visited[to]) continue;
+			visited[to] = ++cnt;
+			q.push(to);
+		}
+	}
+}
+int main() {
+	input();
+
+	for (int i = 1; i <= N; i++)sort(edges[i].begin(), edges[i].end(), greater<int>());
+
+	bfs();
+
+	for (int i = 1; i <= N; i++) cout << visited[i] << "\n";
+	return 0;
+}
