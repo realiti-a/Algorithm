a156e6490b8df2cd8d0a82bed9ec964ada6fb25f
realiti-a <98154578+realiti-a@users.noreply.github.com>
2022-05-20 22:51:56 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int arr[10] = {};
+int visited[10] = {};
+int path[10] = {};
+
+void run(int level, int start) {
+	if (level == M) {
+		for (int i = 0; i < M; i++) cout << path[i] << " ";
+		cout << "\n";
+		return;
+	}
+	int before = 0;
+	for (int i = start; i < N; i++) {
+		int now = arr[i];
+		if (before == now) continue;
+		if (visited[i]) continue;
+		before = now;
+		path[level] = now;
+		run(level + 1, i + 1);
+		path[level] = 0;
+	}
+}
+
+int main() {
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) cin >> arr[i];
+	sort(arr, arr + N);
+	run(0, 0);
+
+	return 0;
+}
